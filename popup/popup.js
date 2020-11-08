9c3b460a484b48a54e234a518b645177b8e0393f
Yu Pan <70486009+yu-pan-cpu@users.noreply.github.com>
2020-11-08 03:34:53 -0500

    Update popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -222,3 +222,12 @@ $(function(){
       })
     })
 })
+
+chrome.runtime.onMessage.addListener(getMessage);
+
+function getMessage(message, sender, sendResponse) {
+  console.log(message);
+
+  speak(message);
+  
+}
